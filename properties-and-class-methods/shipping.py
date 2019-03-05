fb083ba4089e69ff48d335864dbb9da9d932ce41
Johnny Fang <fangdejavu@gmail.com>
2019-03-04 21:59:31 -0500

    static methods with inheritance

--- properties-and-class-methods/shipping.py
+++ properties-and-class-methods/shipping.py
@@ -28,9 +28,23 @@ class ShippingContainer:
     def __init__(self, owner_code, contents):
         self.owner_code = owner_code
         self.contents = contents
-        self.bic = ShippingContainer._make_bic_code(
+        # self.bic = ShippingContainer._make_bic_code(
+        self.bic = self._make_bic_code(  # by changing this to self we get polymorphic BIC generation from the single
+            # constructor implementation
             owner_code=owner_code,
             serial=ShippingContainer._get_next_serial())
         # ShippingContainer.next_serial += 1
         # using self.next_serial += 1 will not have the desired effect bc assignment to attributes
         # (e.g. self.attr = something) always create an instance attribute, never a class attribute
+
+        # keep in mind that by calling static methods through the class we effectively prevent them being overridden,
+        #  at least from the point of view of the base class.
+        # if you need polymorphic dispatch of @staticmethod invocations, call through the self instance
+
+class RefrigeratedShippingContainer(ShippingContainer):
+
+    @staticmethod
+    def _make_bic_code(owner_code, serial):
+        return iso6346.create(owner_code=owner_code,
+                              serial=str(serial).zfill(6),
+                              category='R')
